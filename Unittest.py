41ce01dfa706c40e857146e39ec50cbcd0ed5568
habibkmhd <122400503+habibkmhd@users.noreply.github.com>
2023-02-19 19:10:18 +0100

    Update Unittest.py

--- Unittest.py
+++ Unittest.py
@@ -5,6 +5,9 @@ class MyTestCase(unittest.TestCase):
 
     def test_failed(self):
         self.assertEqual(False,'Failure Message')
+        
+    def testEqual(self):
+        self.assertEqual(8, 13 - 5)
 
 
 if __name__ == '__main__':
