88377e83a8feb1f11c7db76d5fdaabe9c8b5e24e
habibkmhd <122400503+habibkmhd@users.noreply.github.com>
2023-02-19 20:41:52 +0100

    Update Unittest

--- Unittest.py
+++ Unittest.py
@@ -1,14 +1,27 @@
 import unittest
 class MyTestCase(unittest.TestCase):
     def test_success(self):
-        self.assertEqual(True,'Success Message') # add assertion here
+        self.assertEqual(True,True) # add assertion here
 
     def test_failed(self):
-        self.assertEqual(False,'Failure Message')
-        
+        self.assertNotEqual(False,'Failure Message')
+
     def testEqual(self):
         self.assertEqual(8, 13 - 5)
 
+    def test_upper(self):
+        self.assertEqual('foo'.upper(), 'FOO')
+
+    def test_isupper(self):
+        self.assertTrue('FOO'.isupper())
+        self.assertFalse('Foo'.isupper())
+
+    def test_split(self):
+        s = 'hello world'
+        self.assertEqual(s.split(), ['hello', 'world'])
+        # check that s.split fails when the separator is not a string
+        with self.assertRaises(TypeError):
+            s.split(2)
 
 if __name__ == '__main__':
     unittest.main()
